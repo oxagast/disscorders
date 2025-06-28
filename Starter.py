275f8453846665be2a78f37ecc3bef84ff141bcd
Holden Blair <holden.blairpeace17@gmail.com>
2025-06-28 14:43:35 -0400

    Updated Starter.py to fix the code that I broke XD

--- Starter.py
+++ Starter.py
@@ -4,24 +4,22 @@ import time as t
 from discord import app_commands, Interaction, Embed
 
 # Setup Credentials
-BOT_TOKEN = 'Bot_Token_Here'  # Replace With Your Bot Token
+BOT_TOKEN = 'Bot_Token_Here' # Replace With Bot Token
+GUILD_ID = 12345678912345 # Replace With Your Sever ID
 
 # Setup
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+tree = discord.app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print(f"Bot is ready. Logged in as {client.user} (ID: {client.user.id})")
-    print("Syncing commands to all guilds...")
-    for guild in client.guilds:
-        await tree.sync(guild=discord.Object(id=guild.id))
-    print("Synced commands to all guilds.")
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
 
 # Commands
-@tree.command(name="ping", description="sends ping of bot")
-async def ping(interaction: Interaction):
+@tree.command(name="ping", description="sends ping of bot", guild=discord.Object(id=GUILD_ID))
+async def ping(interaction: discord.Interaction):
     latency = client.latency * 1000  # Convert to ms
     await interaction.response.send_message(f'Pong! `{latency:.2f}ms`', ephemeral=True)
 
