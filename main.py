eec075ef18e2acab6f09a7beea0f1c7f4f57ff22
Marshall Whittaker <marshall@oxasploits.com>
2025-08-30 15:25:32 -0400

    Added a base64 encoding command.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import discord
 import time as t
+import base64
 import configparser
+from discord.ext import commands
 from discord import app_commands, Interaction, Embed
 botconfig = configparser.ConfigParser()
 botconfig.read('conf.ini')
@@ -22,6 +24,15 @@ async def on_ready():
 @tree.command(name="ping", description="sends ping of bot", guild=discord.Object(id=GUILD_ID))
 async def ping(interaction: discord.Interaction):
     latency = client.latency * 1000  # Convert to ms
+    print(f"Responding to command (ping).")
     await interaction.response.send_message(f'Pong! `{latency:.2f}ms`', ephemeral=True)
+
+
+@tree.command(name="eb64", description="Encodes Base64", guild=discord.Object(id=GUILD_ID))
+async def base64e(interaction: discord.Interaction, message_text: str):
+    data = message_text.encode("utf-8")
+    b64encoded = base64.b64encode(data)
+    print(f"Responding to command (eb64).")
+    await interaction.response.send_message("Encoded base64: " + b64encoded.decode('utf-8'), ephemeral=True)
 
 client.run(BOT_TOKEN)
